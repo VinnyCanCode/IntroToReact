68b18d059a92da6d5bb3a3f8742db4f5dd2819b0
VincentCanCode <49010435+VinnyCanCode@users.noreply.github.com>
2019-08-06 09:32:54 -0400

    Add files via upload

---
+++ precache-manifest.a131432332a42b079068b8cb77f1ff28.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edba1c3e2d080fbaa220764a09c3a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b38696e82ebc9f2b0484",
+    "url": "/static/js/2.2ee4de2a.chunk.js"
+  },
+  {
+    "revision": "4832988a43240e737fc4",
+    "url": "/static/js/main.243b9363.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
